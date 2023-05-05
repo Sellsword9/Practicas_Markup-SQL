dc4bc31efc248f02fe6a02a0fa02fa70ff7dd57e
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-05 09:02:51 +0200

    Refactoring SQL lines

--- SQL/DeClase/T6/Base.sql
+++ SQL/DeClase/T6/Base.sql
@@ -81,77 +81,30 @@ INSERT INTO ponente VALUES ('USA002', 'Robert', 'Craig', NULL, 'Bases de Datos')
 INSERT INTO ponente VALUES ('ESP004', 'Luisa', 'Soriano', 'López', 'Seguridad Informática');
 INSERT INTO ponente VALUES ('USA003', 'Kevin', 'Shull', NULL, 'Programación');
 
-INSERT INTO participa VALUES ('ESP001','BDO1314',1,450.00);
-INSERT INTO participa VALUES ('USA002','BDO1314',2,350.00);
-INSERT INTO participa VALUES ('ESP002','BDO1314',3,550.00);
-INSERT INTO participa VALUES ('USA003','POO1314',1,600.00);
-INSERT INTO participa VALUES ('USA001','POO1314',2,520.00);
-INSERT INTO participa VALUES ('USA001','PWB1314',1,370.00);
-INSERT INTO participa VALUES ('ESP003','PWB1314',2,250.00);
-INSERT INTO participa VALUES ('FRA001','PWB1314',3,375.00);
-INSERT INTO participa VALUES ('USA003','PWB1314',4,350.00);
-INSERT INTO participa VALUES ('ESP002','ADS1314',1,420.00);
-INSERT INTO participa VALUES ('FRA001','ADS1314',2,330.00);
-INSERT INTO participa VALUES ('USA001','ADS1314',3,560.00);
-INSERT INTO participa VALUES ('USA002','ADS1314',4,150.00);
-INSERT INTO participa VALUES ('ESP004','ADS1314',5,480.00);
-INSERT INTO participa VALUES ('ESP003','SEG1314',1,350.00);
-INSERT INTO participa VALUES ('ESP004','SEG1314',2,290.00);
-INSERT INTO participa VALUES ('USA001','SEG1314',3,600.00);
-INSERT INTO participa VALUES ('FRA001','SEG1314',4,570.00);
+INSERT INTO participa VALUES 
+('ESP001','BDO1314',1,450.00), ('USA002','BDO1314',2,350.00), ('ESP002','BDO1314',3,550.00),
+('USA001','POO1314',2,520.00), ('USA001','PWB1314',1,370.00), ('ESP003','PWB1314',2,250.00), 
+('FRA001','PWB1314',3,375.00), ('USA003','PWB1314',4,350.00), ('ESP002','ADS1314',1,420.00), 
+('FRA001','ADS1314',2,330.00), ('USA001','ADS1314',3,560.00), ('USA002','ADS1314',4,150.00), 
+('ESP004','ADS1314',5,480.00), ('ESP003','SEG1314',1,350.00), ('ESP004','SEG1314',2,290.00),
+('USA001','SEG1314',3,600.00), ('FRA001','SEG1314',4,570.00), ('USA003','POO1314',1,600.00);
 
-INSERT INTO asistente VALUES ('AS0001', 'Mario', 'Céspedes', 'Hermida', 'H', '1970-11-16', 'BK Programación');
-INSERT INTO asistente VALUES ('AS0002', 'Carmen', 'Gutiérrez', 'Sevilla', 'M', '1968-02-20', 'BK Programación');
-INSERT INTO asistente VALUES ('AS0003', 'Felipe', 'Martín', 'Comillas', 'H', '1975-11-06', 'ProgConsulting');
-INSERT INTO asistente VALUES ('AS0004', 'Lourdes', 'Soriano', 'López', 'M', '1982-07-30', NULL);
-INSERT INTO asistente VALUES ('AS0005', 'Inmaculada', 'Herrero', 'Puig', 'M', '1978-09-13', 'BigSoft');
-INSERT INTO asistente VALUES ('AS0006', 'Lucía', 'Díaz', 'Martínez', 'M', '1973-08-25', 'BigSoft');
-INSERT INTO asistente VALUES ('AS0007', 'Fermín', 'Gutiérrez', 'Pérez', 'H', '1969-03-03', 'ProgConsulting');
-INSERT INTO asistente VALUES ('AS0008', 'José Enrique', 'Martos', 'Martos', 'H', '1982-10-20', 'BK Programación');
-INSERT INTO asistente VALUES ('AS0009', 'José Ramón', 'Gómez', 'Pérez', 'H', '1973-01-21', 'ProgConsulting');
-INSERT INTO asistente VALUES ('AS0010', 'Mercedes', 'Bosh', 'Toral', 'M', '1980-05-05', 'BK Programación');
-INSERT INTO asistente VALUES ('AS0011', 'María José', 'Vázquez', 'Soriano', 'M', '1990-03-19', NULL);
-INSERT INTO asistente VALUES ('AS0012', 'José Luis', 'Jiménez', 'Molina', 'H', '1991-10-01', 'BigSoft');
-INSERT INTO asistente VALUES ('AS0013', 'Manuel', 'Pérez', 'Martínez', 'H', '1987-11-30', NULL);
+INSERT INTO asistente VALUES ('AS0001', 'Mario', 'Céspedes', 'Hermida', 'H', '1970-11-16', 'BK Programación'),
+('AS0002', 'Carmen', 'Gutiérrez', 'Sevilla', 'M', '1968-02-20', 'BK Programación'  ) , ('AS0003', 'Felipe', 'Martín', 'Comillas', 'H', '1975-11-06', 'ProgConsulting')
+('AS0004', 'Lourdes', 'Soriano', 'López', 'M', '1982-07-30', NULL                  ) , ('AS0005', 'Inmaculada', 'Herrero', 'Puig', 'M', '1978-09-13', 'BigSoft')
+('AS0006', 'Lucía', 'Díaz', 'Martínez', 'M', '1973-08-25', 'BigSoft'               ) , ('AS0007', 'Fermín', 'Gutiérrez', 'Pérez', 'H', '1969-03-03', 'ProgConsulting')
+('AS0008', 'José Enrique', 'Martos', 'Martos', 'H', '1982-10-20', 'BK Programación') , ('AS0009', 'José Ramón', 'Gómez', 'Pérez', 'H', '1973-01-21', 'ProgConsulting')
+('AS0010', 'Mercedes', 'Bosh', 'Toral', 'M', '1980-05-05', 'BK Programación'       ) , ('AS0011', 'María José', 'Vázquez', 'Soriano', 'M', '1990-03-19', NULL)
+('AS0012', 'José Luis', 'Jiménez', 'Molina', 'H', '1991-10-01', 'BigSoft'          ) , ('AS0013', 'Manuel', 'Pérez', 'Martínez', 'H', '1987-11-30', NULL);
 
-INSERT INTO asiste VALUES ('AS0013', 'BDO1314');
-INSERT INTO asiste VALUES ('AS0009', 'BDO1314');
-INSERT INTO asiste VALUES ('AS0006', 'BDO1314');
-INSERT INTO asiste VALUES ('AS0007', 'BDO1314');
-INSERT INTO asiste VALUES ('AS0012', 'BDO1314');
-INSERT INTO asiste VALUES ('AS0003', 'BDO1314');
-INSERT INTO asiste VALUES ('AS0008', 'POO1314');
-INSERT INTO asiste VALUES ('AS0003', 'POO1314');
-INSERT INTO asiste VALUES ('AS0005', 'POO1314');
-INSERT INTO asiste VALUES ('AS0013', 'POO1314');
-INSERT INTO asiste VALUES ('AS0002', 'POO1314');
-INSERT INTO asiste VALUES ('AS0001', 'POO1314');
-INSERT INTO asiste VALUES ('AS0012', 'POO1314');
-INSERT INTO asiste VALUES ('AS0010', 'POO1314');
-INSERT INTO asiste VALUES ('AS0004', 'POO1314');
-INSERT INTO asiste VALUES ('AS0007', 'POO1314');
-INSERT INTO asiste VALUES ('AS0011', 'POO1314');
-INSERT INTO asiste VALUES ('AS0012', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0007', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0009', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0013', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0008', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0004', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0011', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0003', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0010', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0001', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0006', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0005', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0002', 'PWB1314');
-INSERT INTO asiste VALUES ('AS0012', 'ADS1314');
-INSERT INTO asiste VALUES ('AS0008', 'ADS1314');
-INSERT INTO asiste VALUES ('AS0010', 'ADS1314');
-INSERT INTO asiste VALUES ('AS0001', 'ADS1314');
-INSERT INTO asiste VALUES ('AS0007', 'ADS1314');
-INSERT INTO asiste VALUES ('AS0009', 'SEG1314');
-INSERT INTO asiste VALUES ('AS0008', 'SEG1314');
-INSERT INTO asiste VALUES ('AS0003', 'SEG1314');
-INSERT INTO asiste VALUES ('AS0004', 'SEG1314');
-INSERT INTO asiste VALUES ('AS0010', 'SEG1314');
-INSERT INTO asiste VALUES ('AS0006', 'SEG1314');
+INSERT INTO asiste VALUES ('AS0013', 'BDO1314'), ('AS0009', 'BDO1314'), ('AS0006', 'BDO1314'),
+('AS0007', 'BDO1314'), ('AS0012', 'BDO1314'), ('AS0003', 'BDO1314'), ('AS0008', 'POO1314'),
+('AS0003', 'POO1314'), ('AS0005', 'POO1314'), ('AS0013', 'POO1314'), ('AS0002', 'POO1314'),
+('AS0001', 'POO1314'), ('AS0012', 'POO1314'), ('AS0010', 'POO1314'), ('AS0004', 'POO1314'),
+('AS0007', 'POO1314'), ('AS0011', 'POO1314'), ('AS0012', 'PWB1314'), ('AS0007', 'PWB1314'),
+('AS0009', 'PWB1314'), ('AS0013', 'PWB1314'), ('AS0008', 'PWB1314'), ('AS0004', 'PWB1314'),
+('AS0011', 'PWB1314'), ('AS0003', 'PWB1314'), ('AS0010', 'PWB1314'), ('AS0001', 'PWB1314'),
+('AS0006', 'PWB1314'), ('AS0005', 'PWB1314'), ('AS0002', 'PWB1314'), ('AS0012', 'ADS1314'),
+('AS0008', 'ADS1314'), ('AS0010', 'ADS1314'), ('AS0001', 'ADS1314'), ('AS0007', 'ADS1314'),
+('AS0009', 'SEG1314'), ('AS0008', 'SEG1314'), ('AS0003', 'SEG1314'), ('AS0004', 'SEG1314'),
+('AS0010', 'SEG1314'), ('AS0006', 'SEG1314');
