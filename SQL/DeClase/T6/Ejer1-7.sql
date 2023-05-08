61caf9a48a7d173b52233375e16dce6863d75992
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-08 22:02:47 +0200

    Avanzando

---
+++ SQL/DeClase/T6/Ejer1-7.sql
@@ -0,0 +1,14 @@
+-- Ej 1
+DROP PROCEDURE IF EXISTS getConferencias;
+delimiter $$
+CREATE PROCEDURE getConferencias(in n VARCHAR(50), in a1 VARCHAR(50), in a2 VARCHAR(50))
+BEGIN
+    DECLARE v_ponente CHAR(6);
+    SELECT idPonente into v_ponente FROM ponente WHERE nombre=n AND apellido1 = a1 AND apellido2 = a2;
+    SELECT * FROM conferencia WHERE idConferencia IN ( SELECT idConferencia FROM participa WHERE idPonente = v_ponente );
+END $$
+
+Delimiter ;
+
+Call getConferencias('Luisa', 'Soriano', 'López');
+-- Ej 2
