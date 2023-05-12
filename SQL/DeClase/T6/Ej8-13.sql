a183d1e201ccc3ab55292319a8ecec9156261a56
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-12 22:35:24 +0200

    Ej11

--- SQL/DeClase/T6/Ej8-13.sql
+++ SQL/DeClase/T6/Ej8-13.sql
@@ -15,6 +15,28 @@ SET @ap1 = 'Soriano';
 SET @ap2 = 'López';
 EXECUTE sentPreparada USING @nombre, @ap1, @ap2;
 DEALLOCATE PREPARE sentPreparada;
+DROP PROCEDURE IF EXISTS getConferencias;
 -- Ej 9
 -- No entiendo el enunciado
 set @sentencia = select * from conferencia limit ?; PREPARE sente from sentencia; EXECUTE sente using 2; --????
+-- Ej 11
+DROP PROCEDURE if EXISTS howManyConferencias;
+DELIMITER $$
+CREATE PROCEDURE howManyConferencias(IN dia DATE)
+BEGIN
+    DECLARE c_count INT;
+    SELECT COUNT(*) INTO c_count FROM conferencia WHERE fecha = dia;
+    IF c_count = 0 OR c_count IS NULL THEN
+        SET c_count = -1;
+    END IF;
+    SELECT c_count AS 'Conferencias ese dia';
+END $$
+DELIMITER ;
+
+SET @sentencia = 'CALL howManyConferencias(?)';
+PREPARE sentPreparada FROM @sentencia;
+
+SET @dia = DATE('2013-10-3');
+EXECUTE sentPreparada USING @dia;
+
+DEALLOCATE PREPARE sentPreparada;
