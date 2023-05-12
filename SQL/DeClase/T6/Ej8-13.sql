0c00834e922c1a53c1254a701161cb22f4a43f12
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-12 22:14:52 +0200

    Promps y 8

---
+++ SQL/DeClase/T6/Ej8-13.sql
@@ -0,0 +1,16 @@
+DROP PROCEDURE if EXISTS getConferencias;
+delimiter $$
+CREATE PROCEDURE getConferencias(IN n VARCHAR(50), IN a1 VARCHAR(50), IN a2 VARCHAR(50))
+BEGIN
+    DECLARE v_ponente CHAR(6);
+    SELECT idPonente into v_ponente FROM ponente WHERE nombre=n AND apellido1 = a1 AND apellido2 = a2;
+    SELECT * FROM conferencia WHERE idConferencia IN ( SELECT idConferencia FROM participa WHERE idPonente = v_ponente );
+END $$
+delimiter ;
+SET @sentencia = 'CALL getConferencias(?, ?, ?)';
+PREPARE sentPreparada FROM @sentencia;
+SET @nombre = 'Luisa';
+SET @ap1 = 'Soriano';
+SET @ap2 = 'López';
+EXECUTE sentPreparada USING @nombre, @ap1, @ap2;
+DEALLOCATE PREPARE sentPreparada;
---
+++ SQL/DeClase/T6/prompts8-13.md
@@ -0,0 +1,6 @@
+8. Crear una sentencia preparada que devuelva las conferencias en las que participa un ponente. La sentencia se
+   ejecutará utilizando como parámetros el nombre y apellidos del ponente. No olvidar liberar la memoria
+   utilizada por la sentencia preparada.
+9. Crear un procedimiento que devuelva el número de conferencias indicado en un parámetro. También se le
+   pasará otro parámetro con las columnas que debe mostrar de la forma 'col1, col3, col5'. Poner varios ejemplos
+   de llamada al procedimiento.
