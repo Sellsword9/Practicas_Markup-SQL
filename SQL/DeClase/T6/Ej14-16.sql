37c30df291d5ab6f4323b2986cc922b6d91ba1ed
Sellsword9 <yerayromeroortiz@gmail.com>
2023-05-18 22:21:59 +0200

    Ejs 14-16

--- SQL/DeClase/T6/Ej14-16.sql
+++ SQL/DeClase/T6/Ej14-16.sql
@@ -1,41 +1,75 @@
 -- Ej 14
-DELIMITER //
-CREATE PROCEDURE ObtenerSumaCapacidades(IN salas VARCHAR(255), OUT sumaCapacidades INT)
+USE conferencias2;
+DROP PROCEDURE if EXISTS getSumaCapacidades;
+DELIMITER $$
+CREATE PROCEDURE getSumaCapacidades(IN salas VARCHAR(255), OUT sumaCapacidades INT)
 BEGIN
-    DECLARE sala VARCHAR(255);
-    DECLARE capacidad INT;
-    DECLARE total INT DEFAULT 0;
-    DECLARE idx INT DEFAULT 1;
-    DECLARE len INT;
+	 DECLARE INDEXer INT;
+    SELECT sum(capacidad) INTO sumaCapacidades FROM sala WHERE salas LIKE CONCAT("%", nombreSala, "%");
+    if sumaCapacidades IS NULL then sumaCapacidades = -1 /* El procedimiento no es perfecto, pero me falta tiempo*/
+END $$
 
-    SET len = CHAR_LENGTH(salas);
+call getSumaCapacidades('Zeus, Hermes', @resultado);
+SELECT @resultado AS 'Resultado';
 
-    IF len > 0 THEN
-        WHILE idx <= len DO
-            SET sala = TRIM(SUBSTRING(salas, idx, IF(LOCATE(',', salas, idx + 1) > 0, LOCATE(',', salas, idx + 1) - idx - 1, len - idx + 1)));
+DROP PROCEDURE if EXISTS getSumaCapacidades;
+-- Ej 15
+USE conferencias2;
+DROP FUNCTION if exists getNumeroConferenciasDeSala;
 
-            IF sala = '' THEN
-                SET idx = idx + 1;
-                ITERATE;
-            END IF;
+DELIMITER $$
 
-            SET capacidad = (SELECT capacidad FROM salas WHERE nombreSala = sala);
-
-            IF capacidad IS NULL THEN
-                SELECT CONCAT('La sala "', sala, '" no existe') AS mensaje;
-            ELSE
-                SET total = total + capacidad;
-            END IF;
-
-            SET idx = LOCATE(',', salas, idx + 1) + 1;
-        END WHILE;
-
-        SET sumaCapacidades = total;
-    ELSE
-        SET sumaCapacidades = -1;
-    END IF;
-END //
+CREATE FUNCTION getNumeroConferenciasDeSala(nombreDeSala VARCHAR(50)) RETURNS int
+BEGIN
+    DECLARE confs INT;
+    
+   SELECT count(idConferencia) INTO confs FROM conferencia WHERE nombreSala = nombreDeSala; 
+    
+    RETURN confs;
+END $$
 
 DELIMITER ;
 
-select ObtenerSumaCapacidades('Zeus', 'Hermes');
+SELECT getNumeroConferenciasDeSala("Afrodita") as 'Numero de conferencias de Afrodita';
+
+SELECT getNumeroConferenciasDeSala("Apolo") as 'Numero de conferencias de Apolo';
+
+SELECT getNumeroConferenciasDeSala("Zeus") as 'Numero de conferencias de Zeus';
+
+SELECT getNumeroConferenciasDeSala("Hermes") as 'Numero de conferencias de Hermes';
+
+DROP FUNCTION if exists getNumeroConferenciasDeSala;
+
+-- Ej 16
+USE conferencias2;
+DROP FUNCTION if exists getNumeroConferenciasPonente;
+
+delimiter $
+
+CREATE FUNCTION getNumeroConferenciasPonente(idP CHAR(6)) RETURNS INT
+BEGIN 
+DECLARE confs INT;
+SELECT count(distinct(idConferencia)) INTO confs FROM participa WHERE idPonente = idP;
+if confs = 0 or confs is null then set confs = -1; END if;
+RETURN confs;
+END $
+
+SET @ej16 = 81828123;
+SELECT getNumeroConferenciasPonente("ESP001") INTO @ej16;
+SELECT @ej16;
+
+SELECT getNumeroConferenciasPonente("ESP002");
+
+select getNumeroConferenciasPonente("ESP003");
+
+select getNumeroConferenciasPonente("ESP004");
+
+SELECT getNumeroConferenciasPonente("USA001");
+
+SELECT getNumeroConferenciasPonente("USA003");
+
+SELECT getNumeroConferenciasPonente("USA002");
+
+SELECT getNumeroConferenciasPonente("FRA001");
+
+DROP FUNCTION if exists getNumeroConferenciasPonente;
