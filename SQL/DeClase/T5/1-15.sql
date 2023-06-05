da2d212b84dfd98504ca859ad2aa9f2ecf55ef17
Sellsword9 <yerayromeroortiz@gmail.com>
2023-06-05 10:18:40 +0200

    Trabajando en el T5

--- SQL/DeClase/T5/1-15.sql
+++ SQL/DeClase/T5/1-15.sql
@@ -8,15 +8,6 @@ INSERT INTO producto VALUES (12, "Portátil UltraTurbo ejemplo 3000", 800, 11);
 INSERT INTO producto (nombre, precio, id_fabricante) VALUES ("Servicio de asistencia vitalicio", 500, 12);
 INSERT INTO producto (nombre, precio, id_fabricante) VALUES ("Pórtatil básico tp-link", 400, 12);
 -- Ej 5
-CREATE TABLE fabricante_productos (
-    id_fabricante INT UNSIGNED NOT NULL PRIMARY KEY,
-    nombre_producto INT UNSIGNED NOT NULL,
-    precio int UNSIGNED NOT NULL,
-    FOREIGN KEY (id_fabricante) REFERENCES fabricante(id),
-    FOREIGN KEY (nombre_producto) REFERENCES producto(nombre),
-    FOREIGN KEY (precio) REFERENCES producto(precio)
-);
--- Ej 6
 DROP TABLE IF EXISTS fabricante_productos;
 CREATE TABLE fabricante_productos (
     nombre_fabricante VARCHAR(30),
@@ -26,3 +17,13 @@ CREATE TABLE fabricante_productos (
 INSERT INTO fabricante_productos 
 SELECT fabricante.nombre, producto.nombre, producto.precio 
 FROM fabricante JOIN producto ON fabricante.id = producto.id_fabricante;
+-- Ej 6
+CREATE VIEW vista_fabricante_producto AS
+SELECT fabricante.nombre AS "nombre_fabricante", producto.nombre, producto.precio
+FROM fabricante JOIN producto ON fabricante.id = producto.id_fabricante;
+-- Ej 7
+delete from fabricante where nombre = "Xiomi"; -- Funciona
+ALTER TABLE producto DROP if exists FOREIGN KEY id_fabricante;
+ALTER TABLE producto MODIFY COLUMN id_fabricante INT UNSIGNED ;
+ALTER TABLE producto ADD FOREIGN KEY (id_fabricante) REFERENCES fabricante(id) ON DELETE CASCADE;
+DELETE FROM fabricante WHERE nombre = "Asus"; -- TODO: No funciona
